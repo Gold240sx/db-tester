20cd26b25ba7c7bac1fa5f369a9b5357900acdad
Gold240sx <240designworks@gmail.com>
2023-02-28 16:17:10 -0600

    update to login with animated loading

--- src/components/authForms/signup/signUpFormVanilla.jsx
+++ src/components/authForms/signup/signUpFormVanilla.jsx
@@ -10,9 +10,10 @@ import { useForm } from "../../../hooks/useForm";
 const SignUpFormVanilla = () => {
   const { database } = useDatabase();
   const { form, authFunction, validation } = useForm();
+  const [loading, setLoading] = useState(false);
   const [functionSelected, setFunctionSelected] = useState();
 
-  const handleSignIn = () => {
+  const handleSignUp = () => {
     console.log("Sign Up with Google");
   };
 
@@ -23,7 +24,7 @@ const SignUpFormVanilla = () => {
       </h1>
       <form className="relative flex flex-col gap-3">
         <section>
-          <h2 className=" ml-5 text-gray-400">...Select a login provider:</h2>
+          <h2 className=" ml-2 text-gray-400">Select a login provider:</h2>
           <div
             id="alt-login-options"
             className=" mt-4 mb-4 grid grid-cols-2 gap-2"
@@ -31,7 +32,7 @@ const SignUpFormVanilla = () => {
             <button
               type="button"
               className="dark:focus:ring-[#4285F4]/55 mr-2 mb-2 inline-flex items-center whitespace-nowrap rounded-lg bg-[#4285F4] px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-[#4285F4]/90 focus:outline-none focus:ring-4 focus:ring-[#4285F4]/50"
-              onClick={handleSignIn}
+              onClick={handleSignUp}
             >
               <svg
                 className="mr-2 -ml-1 h-4 w-4"
@@ -53,7 +54,7 @@ const SignUpFormVanilla = () => {
             <button
               type="button"
               className="mr-2 mb-2 inline-flex items-center whitespace-nowrap rounded-lg bg-[#24292F] px-5  py-2.5  text-center text-sm font-medium text-white hover:bg-[#24292F]/90 focus:outline-none focus:ring-4 focus:ring-[#24292F]/50 dark:hover:bg-[#050708]/30 dark:focus:ring-gray-500"
-              onClick={handleSignIn}
+              onClick={handleSignUp}
             >
               <svg
                 className="mr-2 -ml-1 h-4 w-4"
@@ -75,7 +76,7 @@ const SignUpFormVanilla = () => {
             <button
               type="button"
               className="dark:focus:ring-[#3b5998]/55 mr-2 mb-2 inline-flex items-center whitespace-nowrap rounded-lg bg-[#3b5998] px-5  py-2.5  text-center text-sm font-medium text-white hover:bg-[#3b5998]/90 focus:outline-none focus:ring-4 focus:ring-[#3b5998]/50"
-              onClick={handleSignIn}
+              onClick={handleSignUp}
             >
               <svg
                 className="mr-2 -ml-1 h-4 w-4"
@@ -99,7 +100,7 @@ const SignUpFormVanilla = () => {
               className="mr-2 mb-2 inline-flex items-center rounded-lg bg-[#050708] px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-[#050708]/90 focus:outline-none focus:ring-4 focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30 dark:focus:ring-[#050708]/50"
             >
               <svg
-                class="mr-2 -ml-1 h-5 w-5"
+                className="mr-2 -ml-1 h-5 w-5"
                 aria-hidden="true"
                 focusable="false"
                 data-prefix="fab"
@@ -130,7 +131,7 @@ const SignUpFormVanilla = () => {
               type="email"
               name="email"
               id="email"
-              className="rounded-lg border border-gray-300 font-normal focus:border-transparent focus:outline-none focus:ring-2 focus:ring-green-500"
+              className="rounded border border-gray-300 font-normal focus:border-transparent focus:outline-none focus:ring-2 focus:ring-green-500"
             />
           </div>{" "}
           <div className="m-4 flex flex-col">
@@ -141,15 +142,46 @@ const SignUpFormVanilla = () => {
               type="password"
               name="password"
               id="password"
-              className="rounded-lg border border-gray-300 font-normal focus:border-transparent focus:outline-none focus:ring-2 focus:ring-green-500"
+              className="rounded border border-gray-300 font-normal focus:border-transparent focus:outline-none focus:ring-2 focus:ring-green-500"
             />
           </div>
-          <button
-            className="mx-auto my-2 flex rounded-lg bg-green-500 py-1 px-3 text-white"
-            type="submit"
-          >
-            Create Account!
-          </button>
+          <div className="flex flex-col ">
+            <button
+              type="button"
+              class={` ${
+                loading ? "hidden" : "block"
+              } mx-auto w-1/2 rounded-lg bg-blue-700 px-5 py-3 text-center text-base font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 `}
+              onClick={handleSignUp}
+            >
+              Create Account
+            </button>
+            <button
+              disabled
+              type="button"
+              class={` ${
+                loading ? "inline-flex" : "hidden"
+              } mx-auto h-[48px] w-1/2 items-center justify-center rounded-lg border border-gray-200 bg-white py-2.5 px-5 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:text-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-700 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+            >
+              <svg
+                aria-hidden="true"
+                role="status"
+                class="mr-3 inline h-4 w-4 animate-spin text-gray-200 dark:text-gray-600"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="currentColor"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="#1C64F2"
+                />
+              </svg>
+              Loading...
+            </button>
+          </div>
         </section>
       </form>
     </div>
